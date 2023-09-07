8c427f85fbcf120784519eae805a467de4ed0612
Lucas Echebeste <echebesteee.lucas@gmail.com>
2023-09-07 15:27:14 -0300

    first commit

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" className='solar-system'>
+        <Title headline="Planetas" />
+        <div className='plan'>
+          {Planets.map(({ name, image }, index) => (
+            <PlanetCard
+              key={ index }
+              planetName={ name }
+              planetImage={ image }
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
